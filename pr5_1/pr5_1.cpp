8cc4f93df03ea91c979cdddd57f23e54a62c00b0
MISTIKMAKS <mistikmaks80885@gmail.com>
2020-12-03 10:29:56 +0200

    first master commit

---
+++ pr5_1/pr5_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double x, const double y, const double z);
+int main()
+{
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double f = (h(1, t + s, s) + h(t, s * t, 1))/(1 + pow(h(s, 1, t), 2));
+	cout << "f = " << f << endl;
+	return 0;
+}
+double h(const double a, const double b, const double c)
+{
+	return (a * sin(b) + b * sin(a) + c * c);
+}
